1baa20833ead678c2f9dbaf49e881b1f10727148
jschaef80 <jschaef80@gmail.com>
2019-06-04 20:04:53 -0500

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+const state = require('./state.js')
+
+app.use(bodyParser.json())
+app.get('/users', (req, res) => res.send(state.users))
+app.get('/users/1', (req, res) => res.send(state.users[0]))
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
